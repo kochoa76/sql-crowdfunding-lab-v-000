6ee2905af324bdf67279892735efda0d4c0f3cad
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 17:48:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,10 @@ projects.id GROUP BY(projects.title);"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT user.name, user.age, pledges.amount FROM USER 
+INNER JOIN pledges 
+ON pledges.user_id = 
+user.id GROUP BY(user.name)"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
