4f224e2f7f343b51fd308711f07865605e386c2e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 18:09:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,8 +24,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, (SUM(pledges.amount) - (projects.funding_goal) AS amount_left FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-GROUP BY amount_left HAVING
-projects.title >= 0; "
+GROUP BY projects_title HAVING
+amount_left >= 0; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
