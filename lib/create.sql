14fc9c3c4b1aeb56d9583c86e493992e9769e471
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 17:11:39 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,4 +6,5 @@ CREATE TABLE users (id INTEGER PRIMARY KEY,
   name TEXT, age INTEGER);
 
 CREATE TABLE pledges (id INTEGER PRIMARY KEY,
-  amount INTEGER, user_id INTEGER);
+  amount INTEGER, user_id INTEGER,
+  project_id INTEGER);
