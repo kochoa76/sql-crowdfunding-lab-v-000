88c61c10be5ff9a257953e0e4fe29b9a86a4b3e2
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 17:58:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 INNER JOIN pledges
 ON pledges.project_id = projects.id 
 GROUP BY pledges.amount HAVING
-SUM(pledges.amount) > projects.funding_goal; "
+SUM(pledges.amount) >= projects.funding_goal; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
