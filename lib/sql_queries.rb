b30eb053c83618d98e6b512a8a8893dba668fb7f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-28 18:12:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ users.id GROUP BY(users.name)"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (SUM(pledges.amount) - (projects.funding_goal) AS amount_left FROM projects
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY projects_title HAVING
